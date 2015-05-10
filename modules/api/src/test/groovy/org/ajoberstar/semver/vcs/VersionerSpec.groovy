998303fe0aa56c72abf9da1d82ce3bbb9747efaa
Andrew Oberstar <ajoberstar@gmail.com>
2015-05-09 19:12:49 -0500

    More testing of Versioner.

--- modules/api/src/test/groovy/org/ajoberstar/semver/vcs/VersionerSpec.groovy
+++ modules/api/src/test/groovy/org/ajoberstar/semver/vcs/VersionerSpec.groovy
@@ -48,9 +48,59 @@ class VersionerSpec extends Specification {
         MINOR | 'final' | true  | '1.3.0'
         PATCH | 'final' | true  | '1.2.4'
         MAJOR | 'other' | true  | '2.0.0-other.1'
-        PATCH | 'other' | true  | '1.2.4-other.1'
         MINOR | 'other' | true  | '1.3.0-other.1'
+        PATCH | 'other' | true  | '1.2.4-other.1'
         MINOR | 'rc'    | true  | '1.3.0-rc.4'
         MINOR | 'other' | false | '1.3.0-rc.3.other.1'
+    }
+
+    def 'infer determines correct version when no previous version'() {
+        given:
+        provider.previousVersion >> Optional.empty()
+        provider.previousRelease >> Optional.empty()
+        expect:
+        versioner.infer(scope, stage, fixed) == Version.valueOf(expected)
+        where:
+        scope | stage   | fixed | expected
+        MAJOR | 'final' | true  | '1.0.0'
+        MINOR | 'final' | true  | '0.1.0'
+        PATCH | 'final' | true  | '0.0.1'
+        MAJOR | 'other' | true  | '1.0.0-other.1'
+        MINOR | 'other' | true  | '0.1.0-other.1'
+        PATCH | 'other' | true  | '0.0.1-other.1'
+    }
+
+    def 'infer determines correct version when no previous release'() {
+        given:
+        provider.previousVersion >> Optional.of(Version.valueOf('1.0.0-rc.3'))
+        provider.previousRelease >> Optional.empty()
+        expect:
+        versioner.infer(scope, stage, fixed) == Version.valueOf(expected)
+        where:
+        scope | stage   | fixed | expected
+        MAJOR | 'final' | true  | '1.0.0'
+        MINOR | 'final' | true  | '0.1.0'
+        PATCH | 'final' | true  | '0.0.1'
+        MAJOR | 'other' | true  | '1.0.0-other.1'
+        MINOR | 'other' | true  | '0.1.0-other.1'
+        PATCH | 'other' | true  | '0.0.1-other.1'
+        MAJOR | 'rc'    | true  | '1.0.0-rc.4'
+        MAJOR | 'other' | false | '1.0.0-rc.3.other.1'
+    }
+
+    def 'infer determines correct version when previous version was release'() {
+        given:
+        provider.previousVersion >> Optional.of(Version.valueOf('1.0.0'))
+        provider.previousRelease >> Optional.of(Version.valueOf('1.0.0'))
+        expect:
+        versioner.infer(scope, stage, fixed) == Version.valueOf(expected)
+        where:
+        scope | stage   | fixed | expected
+        MAJOR | 'final' | true  | '2.0.0'
+        MINOR | 'final' | true  | '1.1.0'
+        PATCH | 'final' | true  | '1.0.1'
+        MAJOR | 'other' | true  | '2.0.0-other.1'
+        MINOR | 'other' | true  | '1.1.0-other.1'
+        PATCH | 'other' | true  | '1.0.1-other.1'
     }
 }
