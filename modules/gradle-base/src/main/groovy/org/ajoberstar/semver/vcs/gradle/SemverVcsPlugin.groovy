101a705fc9802b78985b08db9589a2a418fa856b
Andrew Oberstar <ajoberstar@gmail.com>
2015-05-17 19:22:03 -0500

    Split the Gradle plugins into base and grgit.

--- modules/gradle-base/src/main/groovy/org/ajoberstar/semver/vcs/gradle/SemverVcsPlugin.groovy
+++ modules/gradle-base/src/main/groovy/org/ajoberstar/semver/vcs/gradle/SemverVcsPlugin.groovy
@@ -3,6 +3,9 @@ package org.ajoberstar.semver.vcs.gradle
 import com.github.zafarkhaja.semver.Version
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.ajoberstar.semver.vcs.Scope
+import org.ajoberstar.semver.vcs.Versioners
+import org.ajoberstar.semver.vcs.VersionerBuilder
 
 import java.util.concurrent.atomic.AtomicReference
 
@@ -10,6 +13,28 @@ class SemverVcsPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         def extension = project.extensions.create('semver', SemverExtension)
+
+        VersionerBuilder builder = Versioners.builder()
+
+        Scope scope = Scope.MINOR
+        if (project.hasProperty('semver.scope')) {
+            scope = Scope.valueOf(project.getProperty('semver.scope').toUpperCase())
+        }
+        builder.changeScope(scope)
+
+        if (project.hasProperty('semver.stage')) {
+            String stage = project.getProperty('semver.stage')
+            if (stage == 'final') {
+                builder.finalRelease()
+            } else {
+                builder.fixedStagePreRelease(stage)
+            }
+        } else {
+            builder.floatingStagePreRelease('dev')
+        }
+
+        extension.versioner = builder.build()
+
         def delayedVersion = new DelayedVersion(extension)
         project.allprojects {
             version = delayedVersion
--- modules/gradle-grgit/src/main/groovy/org/ajoberstar/semver/vcs/gradle/grgit/GrgitVcsPlugin.groovy
+++ modules/gradle-grgit/src/main/groovy/org/ajoberstar/semver/vcs/gradle/grgit/GrgitVcsPlugin.groovy
@@ -8,9 +8,9 @@ import org.gradle.api.Project
 class GrgitVcsPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.pluginManager.apply(SemverVcsPlugin)
+        project.pluginManager.apply('org.ajoberstar.grgit')
+        project.pluginManager.apply('org.ajoberstar.semver-vcs-base')
         project.extensions.getByType(SemverExtension).with {
-            Grgit git = Grgit.open(dir: project.rootDir)
             vcs = new GrgitVcs(git)
         }
     }
