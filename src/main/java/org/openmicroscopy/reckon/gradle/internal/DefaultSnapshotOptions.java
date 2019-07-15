57ae789fa39920bcb195549b6ff49dee08f58b3e
Riad Gozim <rgozim1@gmail.com>
2019-07-15 10:17:52 +0100

    updated snapshot settings to fix stage property setting

--- src/main/java/org/openmicroscopy/reckon/gradle/internal/DefaultSnapshotOptions.java
+++ src/main/java/org/openmicroscopy/reckon/gradle/internal/DefaultSnapshotOptions.java
@@ -16,45 +16,19 @@ import java.util.function.BiFunction;
 
 public class DefaultSnapshotOptions extends DefaultStageOptions implements SnapshotOptions {
 
-    final Property<Boolean> snapshot;
-
     @Inject
     public DefaultSnapshotOptions(Project project, @Nullable Map<String, ?> args) {
         super(project, args);
-
-        this.snapshot = project.getObjects().property(Boolean.class);
 
         // Set stages to be snapshot and final
         this.stages.set(Arrays.asList("snapshot", "rc", "final"));
 
         // Default to selecting the snapshot
         this.defaultStage.convention("snapshot");
-        this.snapshot.convention(true);
-    }
-
-    @Override
-    public Property<Boolean> getSnapshot() {
-        return snapshot;
     }
 
     @Override
     public BiFunction<VcsInventory, Version, Optional<String>> evaluateStage() {
-        return (inventory, targetNormal) -> {
-
-            Optional<String> stageProp =
-                    findProperty(ReckonPlugin.STAGE_PROP, defaultStage.get());
-
-            Optional<String> snapshotProp =
-                    findProperty(ReckonPlugin.SNAPSHOT_PROP, snapshot.get())
-                            .map(Boolean::parseBoolean)
-                            .map(isSnapshot -> isSnapshot ? "snapshot" : "final");
-
-            snapshotProp.ifPresent(val -> {
-                project.getLogger().warn("Property {} is deprecated and will be removed in 1.0.0. Use {} set to one of" +
-                        " [snapshot, final].", ReckonPlugin.SNAPSHOT_PROP, ReckonPlugin.STAGE_PROP);
-            });
-
-            return stageProp.isPresent() ? stageProp : snapshotProp;
-        };
+        return (inventory, targetNormal) -> findProperty(ReckonPlugin.STAGE_PROP, null);
     }
 }
