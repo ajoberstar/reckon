8cefbd4e27f47f2f71bc4231c6d41ecdb5600845
Andrew Oberstar <andrew.oberstar@outlook.com>
2021-12-06 10:51:44 -0600

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 pluginManagement {
   plugins {
-    id("com.gradle.plugin-publish") version "0.12.0"
+    id("com.gradle.plugin-publish") version "0.18.0"
 
-    id("org.ajoberstar.grgit") version "4.0.2"
-    id("org.ajoberstar.reckon") version "0.12.0"
-    id("org.ajoberstar.stutter") version "0.5.1"
+    id("org.ajoberstar.grgit") version "4.1.1"
+    id("org.ajoberstar.reckon") version "0.13.0"
+    id("org.ajoberstar.stutter") version "0.6.0"
 
-    id("com.diffplug.spotless") version "5.5.2"
+    id("com.diffplug.spotless") version "6.0.0"
   }
 }
 
