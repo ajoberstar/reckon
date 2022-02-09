0c98281a744d9296a2704e3d8656b79e54fc47e1
Andrew Oberstar <andrew.oberstar@outlook.com>
2022-02-09 08:27:03 -0600

    Use latest defaults plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
   plugins {
-    id("org.ajoberstar.defaults.java-library") version "0.17.0"
-    id("org.ajoberstar.defaults.gradle-plugin") version "0.17.0"
+    id("org.ajoberstar.defaults.java-library") version "0.17.1"
+    id("org.ajoberstar.defaults.gradle-plugin") version "0.17.1"
 
     id("org.ajoberstar.grgit") version "4.1.1"
     id("org.ajoberstar.reckon") version "0.13.1"
