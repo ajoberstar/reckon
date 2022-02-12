e2307fa8c20f11604b2716bf0f773136342bdd89
Andrew Oberstar <andrew.oberstar@outlook.com>
2022-02-12 08:33:54 -0600

    Use latest plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,8 @@ pluginManagement {
     id("org.ajoberstar.defaults.java-library") version "0.17.1"
     id("org.ajoberstar.defaults.gradle-plugin") version "0.17.1"
 
-    id("org.ajoberstar.grgit") version "4.1.1"
-    id("org.ajoberstar.reckon") version "0.13.1"
+    id("org.ajoberstar.grgit") version "5.0.0"
+    id("org.ajoberstar.reckon") version "0.14.0"
     id("org.ajoberstar.stutter") version "0.7.0"
 
     id("com.diffplug.spotless") version "6.2.1"
