fc071b2732c090a03f6bcddb8d99403b0b032114
Andrew Oberstar <andrew.oberstar@outlook.com>
2022-03-27 10:22:59 -0500

    Only use Central for plugins
    Plugin portal had another issue today where it's missing some Jackson jars.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,14 @@ pluginManagement {
     id("org.ajoberstar.defaults.gradle-plugin") version "0.17.1"
 
     id("org.ajoberstar.grgit") version "5.0.0"
-    id("org.ajoberstar.reckon") version "0.16.0"
-    id("org.ajoberstar.stutter") version "0.7.0"
+    id("org.ajoberstar.reckon") version "0.16.1"
+    id("org.ajoberstar.stutter") version "0.7.1"
 
-    id("com.diffplug.spotless") version "6.2.1"
+    id("com.diffplug.spotless") version "6.3.0"
+  }
+
+  repositories {
+    mavenCentral()
   }
 }
 
